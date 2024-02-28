490cc907af43ce502c6b2fc2d8bdd0f0c94115b9
Bruno <bruno5289@hotmail.com>
2024-02-28 19:27:41 -0400

    Update install_dependencies.py
    Explicação do script adicionada

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,3 +1,5 @@
+#Este script cria um subprocesso para instalar as dependências Python
+#listadas no arquivo constraints.txt.
 import subprocess
 
 def install_dependencies_from_txt(file_path):
@@ -6,8 +8,12 @@ def install_dependencies_from_txt(file_path):
         dependencies = [dependency.strip() for dependency in dependencies]
 
     for dependency in dependencies:
+                        #Preparando o comando
         subprocess.call(['pip', 'install', dependency])
 
 if __name__ == "__main__":
     file_path = input("Digite o caminho para o arquivo .txt com as dependências: ")
     install_dependencies_from_txt(file_path)
+    #Se o txt estiver na mesma pasta que este script, insira o nome 
+    #completo do arquivo como resposta no terminal(Ex.: contraints.txt)
+    
